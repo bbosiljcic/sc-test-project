a3f86cf2c26159dcc1ae0f58f6984f171284df1b
Boris Bosiljcic <b.bosiljcic@gmail.com>
2020-09-24 11:39:31 +0200

    rebuild the fe

--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -6,8 +6,23 @@
     <title>Reports by @bbosiljcic</title>
     <link rel="stylesheet" href="/css/app.css">
     <script src="/js/app.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 </head>
 <body>
-<h1>reports</h1>
+    <div class="wrapper">
+        <div class="sidebar">
+            <div class="menu">
+                <div class="logo">
+                    <img src="/assets/storyclash-square-logo.png">
+                </div>
+            </div>
+            <div class="reports">
+                my Reports
+            </div>
+        </div>
+        <div class="main">
+            <h1>reports</h1>
+        </div>
+    </div>
 </body>
 </html>
